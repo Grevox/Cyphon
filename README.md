f89a30f09ad1479718a3cc2a13ae7a85d62dc6f4
Grevox <grevoxyt@gmail.com>
2025-01-07 14:26:21 +0100

    updt

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 <h1>*Siphon or SI*</h1>
 
 <p><strong>SI = Siphon Interpreter <br> this is an interpreted programming language </strong></p>
+<br><br><br>
+<h1><strong><i>PLEASE , DO NOT USE DOTS IN YOU FILES IF NOT FOR THE EXTENSION</i></strong></h1>
+<br>
+<h2>extension = <b>.si</b></h2>
---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include "mainheader.hpp"
+#define FATAL_ERROR -1;
+#define ERROR 1;
+#define EXIT_CODE 0;
+int main(int argc, char *argv[]) {
+	if (argc <= 1) {
+		std::cout << "\nFATAL ERROR: no specifier :\ntry to do: -h to get some more info\n\n";
+		return FATAL_ERROR;
+	}
+	// for now let's only do the interpreter, ok?
+	// it will be a LOOOOONG journey
+	if (argc >= 2) {
+		// argv 1 is what comes after .\a.exe
+		if (strcmp("-h", argv[1]) == 0) {
+			std::cout << "\n-h : get more info\n-o : start interpreter\n-v : version\n\n";
+		}
+		else if (strcmp("-v", argv[1]) == 0) {
+			std::cout << "\nSiphon interpreter  [SI]\nVersion: 0.0.1\n\n";
+		}
+		else if (strcmp("-o", argv[1]) == 0) {
+			if (argv[2] == NULL) {
+				std::cout << "\nFATAL ERROR: no input files\n\n";
+				return FATAL_ERROR;
+			}
+		}
+	}
+	return EXIT_CODE;
+}
